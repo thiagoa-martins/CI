3ac5c8362643f14d45cb55f9ce8c39fbc2766cf7
Thiago <thiagoamartins0@gmail.com>
2025-07-22 20:59:26 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+lista_de_usuarios = ['Thiago', 'Pedro', 'Munir', 'Vinicius', 'Ryan']
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_de_usuarios=lista_de_usuarios)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
