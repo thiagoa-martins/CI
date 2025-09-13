405a411a1d5729e813f84ff1684cff74c35e5b58
Thiago <thiagoamartins0@gmail.com>
2025-09-12 22:11:19 -0300

    database configuration

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormCriarConta, FormLogin
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'chave-padrao')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 lista_de_usuarios = ['Thiago', 'Pedro', 'Munir', 'Vinicius', 'Ryan']
 
@@ -25,6 +27,15 @@ def usuarios():
 def login():
     form_criar_conta = FormCriarConta()
     form_login = FormLogin()
+
+    if form_criar_conta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criada com sucesso para o e-mail: {form_criar_conta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+
     return render_template('login.html', form_criar_conta=form_criar_conta, form_login=form_login)
 
 if __name__ == '__main__':
